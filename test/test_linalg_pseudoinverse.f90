5613b8b895976d424231803b612f5debcc60562e
wassup05 <160420354+wassup05@users.noreply.github.com>
2025-07-05 15:29:57 +0000

    Deploying to stdlib-fpm from @ wassup05/stdlib@3832d5e80886dd8b5b33a2082d2fc34f9bba3031 🚀

--- test/test_linalg_pseudoinverse.f90
+++ test/test_linalg_pseudoinverse.f90
@@ -18,24 +18,24 @@ module test_linalg_pseudoinverse
         
         allocate(tests(0))
 
-        call add_test(tests,new_unittest("s_eye_pseudoinverse",test_s_eye_pseudoinverse))       
-        call add_test(tests,new_unittest("d_eye_pseudoinverse",test_d_eye_pseudoinverse))       
-        call add_test(tests,new_unittest("s_square_pseudoinverse",test_s_square_pseudoinverse))
-        call add_test(tests,new_unittest("s_tall_pseudoinverse",test_s_tall_pseudoinverse))
-        call add_test(tests,new_unittest("s_wide_pseudoinverse",test_s_wide_pseudoinverse))
-        call add_test(tests,new_unittest("s_singular_pseudoinverse",test_s_singular_pseudoinverse))
-        call add_test(tests,new_unittest("d_square_pseudoinverse",test_d_square_pseudoinverse))
-        call add_test(tests,new_unittest("d_tall_pseudoinverse",test_d_tall_pseudoinverse))
-        call add_test(tests,new_unittest("d_wide_pseudoinverse",test_d_wide_pseudoinverse))
-        call add_test(tests,new_unittest("d_singular_pseudoinverse",test_d_singular_pseudoinverse))
-        call add_test(tests,new_unittest("c_square_pseudoinverse",test_c_square_pseudoinverse))
-        call add_test(tests,new_unittest("c_tall_pseudoinverse",test_c_tall_pseudoinverse))
-        call add_test(tests,new_unittest("c_wide_pseudoinverse",test_c_wide_pseudoinverse))
-        call add_test(tests,new_unittest("c_singular_pseudoinverse",test_c_singular_pseudoinverse))
-        call add_test(tests,new_unittest("z_square_pseudoinverse",test_z_square_pseudoinverse))
-        call add_test(tests,new_unittest("z_tall_pseudoinverse",test_z_tall_pseudoinverse))
-        call add_test(tests,new_unittest("z_wide_pseudoinverse",test_z_wide_pseudoinverse))
-        call add_test(tests,new_unittest("z_singular_pseudoinverse",test_z_singular_pseudoinverse))
+        tests = [tests,new_unittest("s_eye_pseudoinverse",test_s_eye_pseudoinverse)]        
+        tests = [tests,new_unittest("d_eye_pseudoinverse",test_d_eye_pseudoinverse)]        
+        tests = [tests,new_unittest("s_square_pseudoinverse",test_s_square_pseudoinverse), &
+                       new_unittest("s_tall_pseudoinverse",test_s_tall_pseudoinverse), &
+                       new_unittest("s_wide_pseudoinverse",test_s_wide_pseudoinverse), &
+                       new_unittest("s_singular_pseudoinverse",test_s_singular_pseudoinverse)]
+        tests = [tests,new_unittest("d_square_pseudoinverse",test_d_square_pseudoinverse), &
+                       new_unittest("d_tall_pseudoinverse",test_d_tall_pseudoinverse), &
+                       new_unittest("d_wide_pseudoinverse",test_d_wide_pseudoinverse), &
+                       new_unittest("d_singular_pseudoinverse",test_d_singular_pseudoinverse)]
+        tests = [tests,new_unittest("c_square_pseudoinverse",test_c_square_pseudoinverse), &
+                       new_unittest("c_tall_pseudoinverse",test_c_tall_pseudoinverse), &
+                       new_unittest("c_wide_pseudoinverse",test_c_wide_pseudoinverse), &
+                       new_unittest("c_singular_pseudoinverse",test_c_singular_pseudoinverse)]
+        tests = [tests,new_unittest("z_square_pseudoinverse",test_z_square_pseudoinverse), &
+                       new_unittest("z_tall_pseudoinverse",test_z_tall_pseudoinverse), &
+                       new_unittest("z_wide_pseudoinverse",test_z_wide_pseudoinverse), &
+                       new_unittest("z_singular_pseudoinverse",test_z_singular_pseudoinverse)]
 
     end subroutine test_pseudoinverse_matrix
 
@@ -47,7 +47,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: i,j
         integer(ilp), parameter :: n = 15_ilp
-        real(sp), parameter :: tol = 1000*sqrt(epsilon(0.0_sp))
+        real(sp), parameter :: tol = 100*sqrt(epsilon(0.0_sp))
 
         real(sp) :: a(n,n),inva(n,n)
 
@@ -86,7 +86,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: i,j
         integer(ilp), parameter :: n = 15_ilp
-        real(dp), parameter :: tol = 1000*sqrt(epsilon(0.0_dp))
+        real(dp), parameter :: tol = 100*sqrt(epsilon(0.0_dp))
 
         real(dp) :: a(n,n),inva(n,n)
 
@@ -128,7 +128,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: n = 10
-        real(sp), parameter :: tol = 1000*sqrt(epsilon(0.0_sp))
+        real(sp), parameter :: tol = 100*sqrt(epsilon(0.0_sp))
         real(sp) :: a(n, n), inva(n, n)
         
         call random_number(a)
@@ -155,7 +155,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: m = 20, n = 10
-        real(sp), parameter :: tol = 1000*sqrt(epsilon(0.0_sp))
+        real(sp), parameter :: tol = 100*sqrt(epsilon(0.0_sp))
         real(sp) :: a(m, n), inva(n, m)
         
         call random_number(a)
@@ -182,7 +182,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: m = 10, n = 20
-        real(sp), parameter :: tol = 1000*sqrt(epsilon(0.0_sp))
+        real(sp), parameter :: tol = 100*sqrt(epsilon(0.0_sp))
         real(sp) :: a(m, n), inva(n, m)
         
         call random_number(a)
@@ -209,7 +209,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: n = 10
-        real(sp), parameter :: tol = 1000*sqrt(epsilon(0.0_sp))
+        real(sp), parameter :: tol = 100*sqrt(epsilon(0.0_sp))
         real(sp) :: a(n, n), inva(n, n)
         
         call random_number(a)
@@ -240,7 +240,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: n = 10
-        real(dp), parameter :: tol = 1000*sqrt(epsilon(0.0_dp))
+        real(dp), parameter :: tol = 100*sqrt(epsilon(0.0_dp))
         real(dp) :: a(n, n), inva(n, n)
         
         call random_number(a)
@@ -267,7 +267,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: m = 20, n = 10
-        real(dp), parameter :: tol = 1000*sqrt(epsilon(0.0_dp))
+        real(dp), parameter :: tol = 100*sqrt(epsilon(0.0_dp))
         real(dp) :: a(m, n), inva(n, m)
         
         call random_number(a)
@@ -294,7 +294,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: m = 10, n = 20
-        real(dp), parameter :: tol = 1000*sqrt(epsilon(0.0_dp))
+        real(dp), parameter :: tol = 100*sqrt(epsilon(0.0_dp))
         real(dp) :: a(m, n), inva(n, m)
         
         call random_number(a)
@@ -321,7 +321,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: n = 10
-        real(dp), parameter :: tol = 1000*sqrt(epsilon(0.0_dp))
+        real(dp), parameter :: tol = 100*sqrt(epsilon(0.0_dp))
         real(dp) :: a(n, n), inva(n, n)
         
         call random_number(a)
@@ -352,7 +352,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: n = 10
-        real(sp), parameter :: tol = 1000*sqrt(epsilon(0.0_sp))
+        real(sp), parameter :: tol = 100*sqrt(epsilon(0.0_sp))
         complex(sp) :: a(n, n), inva(n, n)
         real(sp) :: rea(n, n, 2)
         
@@ -381,7 +381,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: m = 20, n = 10
-        real(sp), parameter :: tol = 1000*sqrt(epsilon(0.0_sp))
+        real(sp), parameter :: tol = 100*sqrt(epsilon(0.0_sp))
         complex(sp) :: a(m, n), inva(n, m)
         real(sp) :: rea(m, n, 2)
         
@@ -410,7 +410,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: m = 10, n = 20
-        real(sp), parameter :: tol = 1000*sqrt(epsilon(0.0_sp))
+        real(sp), parameter :: tol = 100*sqrt(epsilon(0.0_sp))
         complex(sp) :: a(m, n), inva(n, m)
         real(sp) :: rea(m, n, 2)
         
@@ -439,7 +439,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: n = 10
-        real(sp), parameter :: tol = 1000*sqrt(epsilon(0.0_sp))
+        real(sp), parameter :: tol = 100*sqrt(epsilon(0.0_sp))
         complex(sp) :: a(n, n), inva(n, n)
         real(sp) :: rea(n, n, 2)
         
@@ -472,7 +472,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: n = 10
-        real(dp), parameter :: tol = 1000*sqrt(epsilon(0.0_dp))
+        real(dp), parameter :: tol = 100*sqrt(epsilon(0.0_dp))
         complex(dp) :: a(n, n), inva(n, n)
         real(dp) :: rea(n, n, 2)
         
@@ -501,7 +501,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: m = 20, n = 10
-        real(dp), parameter :: tol = 1000*sqrt(epsilon(0.0_dp))
+        real(dp), parameter :: tol = 100*sqrt(epsilon(0.0_dp))
         complex(dp) :: a(m, n), inva(n, m)
         real(dp) :: rea(m, n, 2)
         
@@ -530,7 +530,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: m = 10, n = 20
-        real(dp), parameter :: tol = 1000*sqrt(epsilon(0.0_dp))
+        real(dp), parameter :: tol = 100*sqrt(epsilon(0.0_dp))
         complex(dp) :: a(m, n), inva(n, m)
         real(dp) :: rea(m, n, 2)
         
@@ -559,7 +559,7 @@ module test_linalg_pseudoinverse
 
         integer(ilp) :: failed
         integer(ilp), parameter :: n = 10
-        real(dp), parameter :: tol = 1000*sqrt(epsilon(0.0_dp))
+        real(dp), parameter :: tol = 100*sqrt(epsilon(0.0_dp))
         complex(dp) :: a(n, n), inva(n, n)
         real(dp) :: rea(n, n, 2)
         
@@ -583,27 +583,6 @@ module test_linalg_pseudoinverse
 
     end subroutine test_z_singular_pseudoinverse
 
-
-    ! gcc-15 bugfix utility
-    subroutine add_test(tests,new_test)
-        type(unittest_type), allocatable, intent(inout) :: tests(:)    
-        type(unittest_type), intent(in) :: new_test
-        
-        integer :: n
-        type(unittest_type), allocatable :: new_tests(:)
-        
-        if (allocated(tests)) then 
-            n = size(tests)
-        else
-            n = 0
-        end if
-        
-        allocate(new_tests(n+1))
-        if (n>0) new_tests(1:n) = tests(1:n)
-                 new_tests(1+n) = new_test
-        call move_alloc(from=new_tests,to=tests)        
-        
-    end subroutine add_test
 
 end module test_linalg_pseudoinverse
 
