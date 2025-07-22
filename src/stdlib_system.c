61e8e74f62942599c3333cc65b7a633d3f0034a8
wassup05 <160420354+wassup05@users.noreply.github.com>
2025-07-22 19:47:56 +0000

    Deploying to stdlib-fpm-ilp64 from @ wassup05/stdlib@9d6325e2dfbd3a123e89c146f28922abba7699ee 🚀

--- src/stdlib_system.c
+++ src/stdlib_system.c
@@ -1,6 +1,4 @@
-#include <limits.h>
 #include <stddef.h>
-#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
@@ -17,19 +15,16 @@ char* stdlib_strerror(size_t* len){
     return err;
 }
 
-int stdlib_make_directory(const char* path, mode_t mode){
+int stdlib_make_directory(const char* path){
     int code;
 #ifdef _WIN32
     code = _mkdir(path);
 #else
-    code = mkdir(path, mode);
+    // Default mode 0777
+    code = mkdir(path, 0777);
 #endif /* ifdef _WIN32 */
     
-    if (!code){
-        return 0;
-    }
-
-    return errno;
+    return (!code) ? 0 : errno;
 }
 
 int stdlib_remove_directory(const char* path){
@@ -40,49 +35,5 @@ int stdlib_remove_directory(const char* path){
     code = rmdir(path);
 #endif /* ifdef _WIN32 */
 
-    if (!code){
-        return 0;
-    }
-
-    return errno;
-}
-
-char* stdlib_get_cwd(size_t* len, int* stat){
-    *stat = 0;
-#ifdef _WIN32
-    char* buffer;
-    buffer = _getcwd(NULL, 0);
-
-    if (buffer == NULL) {
-        *stat = errno;
-        return NULL;
-    }
-
-    *len = strlen(buffer);
-    return buffer;
-#else
-    char buffer[PATH_MAX + 1];
-    if (!getcwd(buffer, sizeof(buffer))) {
-        *stat = errno;
-    }
-
-    *len = strlen(buffer);
-
-    char* res = malloc(*len);
-    strncpy(res, buffer, *len);
-
-    return res;
-#endif /* ifdef _WIN32 */
-}
-
-int stdlib_set_cwd(char* path) {
-    int code;
-#ifdef _WIN32
-    code = _chdir(path);
-#else
-    code = chdir(path);
-#endif /* ifdef _WIN32 */
-    
-    if (code == -1) return errno;
-    return 0;
+    return (!code) ? 0 : errno;
 }
