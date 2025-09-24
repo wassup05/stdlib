6112258789ee5dee6d1aa6f1d0b1d12197249163
wassup05 <160420354+wassup05@users.noreply.github.com>
2025-09-24 16:42:26 +0000

    Deploying to stdlib-fpm-ilp64 from @ wassup05/stdlib@7e296bb0a64dd9e89ef49f4d09354e88259c71e7 🚀

--- src/stdlib_system.F90
+++ src/stdlib_system.F90
@@ -2,9 +2,7 @@ module stdlib_system
 use, intrinsic :: iso_c_binding, only : c_int, c_long, c_ptr, c_null_ptr, c_int64_t, c_size_t, &
     c_f_pointer
 use stdlib_kinds, only: int64, dp, c_bool, c_char
-use stdlib_strings, only: to_c_char, find, to_string
-use stdlib_string_type, only: string_type
-use stdlib_optval, only: optval
+use stdlib_strings, only: to_c_char
 use stdlib_error, only: state_type, STDLIB_SUCCESS, STDLIB_FS_ERROR
 implicit none
 private
@@ -85,15 +83,7 @@ public :: wait
 public :: kill
 public :: elapsed
 public :: is_windows
-
-!! Public path related functions and interfaces
-public :: path_sep
-public :: join_path
-public :: operator(/)
-public :: split_path
-public :: base_name
-public :: dir_name
-
+     
 !! version: experimental
 !!
 !! Tests if a given path matches an existing directory.
@@ -109,93 +99,6 @@ public :: dir_name
 !! Windows, and various UNIX-like environments. On unsupported operating systems, the function will return `.false.`.
 !!
 public :: is_directory
-
-!! version: experimental
-!!
-!! Makes an empty directory.
-!! ([Specification](../page/specs/stdlib_system.html#make_directory))
-!!
-!! ### Summary
-!! Creates an empty directory with default permissions.
-!!
-!! ### Description
-!! This function makes an empty directory according to the path provided.
-!! Relative paths are supported. On Windows, paths involving either `/` or `\` are accepted.
-!! An appropriate error message is returned whenever any error occurs.
-!!
-public :: make_directory
-
-!! version: experimental
-!!
-!! Makes an empty directory, also creating all the parent directories required.
-!! ([Specification](../page/specs/stdlib_system.html#make_directory))
-!!
-!! ### Summary
-!! Creates an empty directory with all the parent directories required to do so.
-!!
-!! ### Description
-!! This function makes an empty directory according to the path provided.
-!! It also creates all the necessary parent directories in the path if they do not exist already.
-!! Relative paths are supported.
-!! An appropriate error message is returned whenever any error occurs.
-!!
-public :: make_directory_all
-
-!! version: experimental
-!!
-!! Removes an empty directory.
-!! ([Specification](../page/specs/stdlib_system.html#remove_directory))
-!!
-!! ### Summary
-!! Removes an empty directory.
-!!
-!! ### Description
-!! This function Removes an empty directory according to the path provided.
-!! Relative paths are supported. On Windows paths involving either `/` or `\` are accepted.
-!! An appropriate error message is returned whenever any error occurs.
-!!
-public :: remove_directory
-
-!! version: experimental
-!!
-!! Gets the current working directory of the process
-!! ([Specification](../page/specs/stdlib_system.html#get_cwd))
-!!
-!! ### Summary
-!! Gets the current working directory.
-!!
-!! ### Description
-!! This subroutine gets the current working directory the process is executing from.
-!!
-public :: get_cwd
-
-!! version: experimental
-!!
-!! Sets the current working directory of the process
-!! ([Specification](../page/specs/stdlib_system.html#set_cwd))
-!!
-!! ### Summary
-!! Changes the current working directory to the one specified.
-!!
-!! ### Description
-!! This subroutine sets the current working directory the process is executing from.
-!!
-public :: set_cwd
-
-!! version: experimental
-!!
-!! Gets the size (in bytes) of a file.
-!! ([Specification](../page/specs/stdlib_system.html#get_file_size))
-!!
-!! ### Summary
-!! Gets the size (in bytes) of a file.
-!!
-!! ### Description
-!! This function gets the size (in bytes) of a file in the filesystem.
-!! It follows symbolic links, gives an error if the symbolic link or the `path` points to a directory instead.
-!! It is designed to work across multiple platforms. On Windows, paths with both forward `/` and backward `\` slashes are accepted.
-!!
-public :: get_file_size
 
 !! version: experimental
 !!
@@ -230,21 +133,6 @@ public :: delete_file
 !! On Windows, this is `NUL`. On UNIX-like systems, this is `/dev/null`.
 !!
 public :: null_device
-
-!! version: experimental
-!!
-!! A helper function for returning the `type(state_type)` with the flag `STDLIB_FS_ERROR` set.
-!! ([Specification](../page/specs/stdlib_system.html#FS_ERROR))
-!!
-public :: FS_ERROR
-
-!! version: experimental
-!!
-!! A helper function for returning the `type(state_type)` with the flag `STDLIB_FS_ERROR` set.
-!! It also formats and prefixes the `code` passed to it as the first argument
-!! ([Specification](../page/specs/stdlib_system.html#FS_ERROR_CODE))
-!!
-public :: FS_ERROR_CODE
      
 ! CPU clock ticks storage
 integer, parameter, private :: TICKS = int64
@@ -662,141 +550,6 @@ interface
     
 end interface 
 
-interface join_path
-    !! version: experimental
-    !!
-    !!### Summary
-    !! join the paths provided according to the OS-specific path-separator
-    !! ([Specification](../page/specs/stdlib_system.html#join_path))
-    !!
-    module function join2_char_char(p1, p2) result(path)
-        character(:), allocatable :: path
-        character(*), intent(in) :: p1, p2
-    end function join2_char_char
-
-    module function join2_char_string(p1, p2) result(path)
-        character(:), allocatable :: path
-        character(*), intent(in) :: p1
-        type(string_type), intent(in) :: p2
-    end function join2_char_string
-
-    module function join2_string_char(p1, p2) result(path)
-        type(string_type) :: path
-        type(string_type), intent(in) :: p1
-        character(*), intent(in) :: p2
-    end function join2_string_char
-
-    module function join2_string_string(p1, p2) result(path)
-        type(string_type) :: path
-        type(string_type), intent(in) :: p1, p2
-    end function join2_string_string
-
-    module function joinarr_char(p) result(path)
-        character(:), allocatable :: path
-        character(*), intent(in) :: p(:)
-    end function joinarr_char
-
-    module function joinarr_string(p) result(path)
-        type(string_type) :: path
-        type(string_type), intent(in) :: p(:)
-    end function joinarr_string
-end interface join_path
-
-interface operator(/)
-    !! version: experimental
-    !!
-    !!### Summary
-    !! A binary operator to join the paths provided according to the OS-specific path-separator
-    !! ([Specification](../page/specs/stdlib_system.html#operator(/)))
-    !!
-    module function join_op_char_char(p1, p2) result(path)
-        character(:), allocatable :: path
-        character(*), intent(in) :: p1, p2
-    end function join_op_char_char
-
-    module function join_op_char_string(p1, p2) result(path)
-        character(:), allocatable :: path
-        character(*), intent(in) :: p1
-        type(string_type), intent(in) :: p2
-    end function join_op_char_string
-
-    module function join_op_string_char(p1, p2) result(path)
-        type(string_type) :: path
-        type(string_type), intent(in) :: p1
-        character(*), intent(in) :: p2
-    end function join_op_string_char
-
-    module function join_op_string_string(p1, p2) result(path)
-        type(string_type) :: path
-        type(string_type), intent(in) :: p1, p2
-    end function join_op_string_string
-end interface operator(/)
-
-interface split_path
-    !! version: experimental
-    !!
-    !!### Summary
-    !! splits the path immediately following the final path-separator
-    !! separating into typically a directory and a file name.
-    !! ([Specification](../page/specs/stdlib_system.html#split_path))
-    !!
-    !!### Description
-    !! If the path is empty `head`='.' and tail=''
-    !! If the path only consists of separators, `head` is set to the separator and tail is empty
-    !! If the path is a root directory, `head` is set to that directory and tail is empty
-    !! `head` ends with a path-separator iff the path appears to be a root directory or a child of the root directory
-    module subroutine split_path_char(p, head, tail)
-        character(*), intent(in) :: p
-        character(:), allocatable, intent(out) :: head, tail
-    end subroutine split_path_char
-
-    module subroutine split_path_string(p, head, tail)
-        type(string_type), intent(in) :: p
-        type(string_type), intent(out) :: head, tail
-    end subroutine split_path_string
-end interface split_path
-
-interface base_name
-    !! version: experimental
-    !!
-    !!### Summary
-    !! returns the base name (last component) of the provided path
-    !! ([Specification](../page/specs/stdlib_system.html#base_name))
-    !!
-    !!### Description
-    !! The value returned is the `tail` of the interface `split_path`
-    module function base_name_char(p) result(base)
-        character(:), allocatable :: base
-        character(*), intent(in) :: p
-    end function base_name_char
-
-    module function base_name_string(p) result(base)
-        type(string_type) :: base
-        type(string_type), intent(in) :: p
-    end function base_name_string
-end interface base_name
-
-interface dir_name
-    !! version: experimental
-    !!
-    !!### Summary
-    !! returns everything but the last component of the provided path
-    !! ([Specification](../page/specs/stdlib_system.html#dir_name))
-    !!
-    !!### Description
-    !! The value returned is the `head` of the interface `split_path`
-    module function dir_name_char(p) result(dir)
-        character(:), allocatable :: dir
-        character(*), intent(in) :: p
-    end function dir_name_char
-
-    module function dir_name_string(p) result(dir)
-        type(string_type) :: dir
-        type(string_type), intent(in) :: p
-    end function dir_name_string
-end interface dir_name
-
-
 contains
 
 integer function get_runtime_os() result(os)
@@ -937,229 +690,6 @@ logical function is_directory(path)
     
 end function is_directory
 
-! A Helper function to convert C character arrays to Fortran character strings
-function to_f_char(c_str_ptr, len) result(f_str)
-    type(c_ptr), intent(in) :: c_str_ptr
-    ! length of the string excluding the null character
-    integer(kind=c_size_t), intent(in) :: len
-    character(:), allocatable :: f_str
-
-    integer :: i
-    character(kind=c_char), pointer :: c_str(:)
-
-    call c_f_pointer(c_str_ptr, c_str, [len])
-
-    allocate(character(len=len) :: f_str)
-
-    do concurrent (i=1:len)
-        f_str(i:i) = c_str(i)
-    end do
-end function to_f_char
-
-! A helper function to get the result of the C function `strerror`.
-! `strerror` is a function provided by `<string.h>`. 
-! It returns a string describing the meaning of `errno` in the C header `<errno.h>`
-function c_get_strerror() result(str)
-    character(len=:), allocatable :: str
-
-    interface
-        type(c_ptr) function strerror(len) bind(C, name='stdlib_strerror')
-            import c_size_t, c_ptr
-            implicit none
-            integer(c_size_t), intent(out) :: len
-        end function strerror
-    end interface
-
-    type(c_ptr) :: c_str_ptr
-    integer(c_size_t) :: len
-
-    c_str_ptr = strerror(len)
-
-    str = to_f_char(c_str_ptr, len)
-end function c_get_strerror
-
-!! makes an empty directory
-subroutine make_directory(path, err)
-    character(len=*), intent(in) :: path
-    type(state_type), optional, intent(out) :: err
-
-    integer :: code
-    type(state_type) :: err0
-
-    interface
-        integer function stdlib_make_directory(cpath) bind(C, name='stdlib_make_directory')
-            import c_char
-            character(kind=c_char), intent(in) :: cpath(*)
-        end function stdlib_make_directory
-    end interface
-
-    code = stdlib_make_directory(to_c_char(trim(path)))
-
-    if (code /= 0) then
-        err0 = FS_ERROR_CODE(code, c_get_strerror())
-        call err0%handle(err)
-    end if
-
-end subroutine make_directory
-
-subroutine make_directory_all(path, err)
-    character(len=*), intent(in) :: path
-    type(state_type), optional, intent(out) :: err
-
-    integer :: i, indx
-    type(state_type) :: err0
-    character(len=1) :: sep
-    logical :: is_dir, check_is_dir
-
-    sep = path_sep()
-    i = 1
-    indx = find(path, sep, i)
-    check_is_dir = .true.
-
-    do
-        ! Base case to exit the loop
-        if (indx == 0) then
-            is_dir = is_directory(path)
-
-            if (.not. is_dir) then
-                call make_directory(path, err0)
-
-                if (err0%error()) then
-                    call err0%handle(err)
-                end if
-            end if
-
-            return
-        end if
-
-        if (check_is_dir) then
-            is_dir = is_directory(path(1:indx))
-        end if
-
-        if (.not. is_dir) then
-            ! no need for further `is_dir` checks
-            ! all paths going forward need to be created
-            check_is_dir = .false.
-            call make_directory(path(1:indx), err0)
-
-            if (err0%error()) then
-                call err0%handle(err)
-                return
-            end if
-        end if
-
-        i = i + 1 ! the next occurence of `sep`
-        indx = find(path, sep, i)
-    end do
-end subroutine make_directory_all
-
-!! removes an empty directory
-subroutine remove_directory(path, err)
-    character(len=*), intent(in) :: path
-    type(state_type), optional, intent(out) :: err
-
-    integer :: code
-    type(state_type) :: err0
-
-    interface
-        integer function stdlib_remove_directory(cpath) bind(C, name='stdlib_remove_directory')
-            import c_char
-            character(kind=c_char), intent(in) :: cpath(*)
-        end function stdlib_remove_directory
-    end interface
-
-    code = stdlib_remove_directory(to_c_char(trim(path)))
-
-    if (code /= 0) then
-        err0 = FS_ERROR_CODE(code, c_get_strerror())
-        call err0%handle(err)
-    end if
-
-end subroutine remove_directory
-
-subroutine get_cwd(cwd, err)
-    character(:), allocatable, intent(out) :: cwd
-    type(state_type), optional, intent(out) :: err
-    type(state_type) :: err0
-
-    interface
-        type(c_ptr) function stdlib_get_cwd(len, stat) bind(C, name='stdlib_get_cwd')
-            import c_ptr, c_size_t
-            integer(c_size_t), intent(out) :: len
-            integer :: stat
-        end function stdlib_get_cwd
-    end interface
-
-    type(c_ptr) :: c_str_ptr
-    integer(c_size_t) :: len
-    integer :: stat
-
-    c_str_ptr = stdlib_get_cwd(len, stat)
-
-    if (stat /= 0) then
-        err0 = FS_ERROR_CODE(stat, c_get_strerror())
-        call err0%handle(err)
-    end if
-
-    cwd = to_f_char(c_str_ptr, len)
-
-end subroutine get_cwd
-
-subroutine set_cwd(path, err)
-    character(len=*), intent(in) :: path
-    type(state_type), optional, intent(out) :: err
-    type(state_type) :: err0
-
-    interface
-        integer function stdlib_set_cwd(path) bind(C, name='stdlib_set_cwd')
-            import c_char
-            character(kind=c_char), intent(in) :: path(*)
-        end function stdlib_set_cwd
-    end interface
-
-    integer :: code
-
-    code = stdlib_set_cwd(to_c_char(trim(path)))
-
-    if (code /= 0) then
-        err0 = FS_ERROR_CODE(code, c_get_strerror())
-        call err0%handle(err)
-    end if
-end subroutine set_cwd
-
-integer(int64) function get_file_size(path, err) result(size)
-    character(*), intent(in) :: path
-    type(state_type), optional, intent(out) :: err
-
-    interface
-        integer function stdlib_get_file_size(path, size) bind(C)
-            import c_char, c_int64_t
-            character(kind=c_char), intent(in) :: path(*)
-            integer(c_int64_t), intent(out) :: size
-        end function stdlib_get_file_size
-    end interface
-
-    type(state_type) :: err0
-    integer :: code
-    integer(c_int64_t) :: c_size
-
-    if (is_directory(path)) then
-        err0 = FS_ERROR("Is a directory!")
-        call err0%handle(err)
-        return
-    end if
-
-    code = stdlib_get_file_size(to_c_char(trim(path)), c_size)
-
-    if (code /= 0) then
-        err0 = FS_ERROR_CODE(code, c_get_strerror())
-        call err0%handle(err)
-        return
-    end if
-
-    size = c_size
-end function get_file_size
-
 !> Returns the file path of the null device for the current operating system.
 !>
 !> Version: Helper function.
@@ -1178,13 +708,21 @@ function null_device() result(path)
         
     end interface
     
-    integer(c_size_t) :: len
+    integer(c_size_t) :: i, len
     type(c_ptr) :: c_path_ptr
+    character(kind=c_char), pointer :: c_path(:)    
 
     ! Call the C function to get the null device path and its length
     c_path_ptr = process_null_device(len)
+    call c_f_pointer(c_path_ptr,c_path,[len])
 
-    path = to_f_char(c_path_ptr, len)
+    ! Allocate the Fortran string with the length returned from C
+    allocate(character(len=len) :: path)
+        
+    do concurrent (i=1:len)
+        path(i:i) = c_path(i)
+    end do
+        
 end function null_device
 
 !> Delete a file at the given path.
@@ -1231,43 +769,5 @@ subroutine delete_file(path, err)
         return              
     end if
 end subroutine delete_file
-
-pure function FS_ERROR_CODE(code,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,& 
-        a11,a12,a13,a14,a15,a16,a17,a18,a19) result(state)
-
-    type(state_type) :: state
-    !> Platform specific error code
-    integer, intent(in) :: code
-    !> Optional rank-agnostic arguments
-    class(*), intent(in), optional, dimension(..) :: a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,&
-        a11,a12,a13,a14,a15,a16,a17,a18,a19
-
-    character(32) :: code_msg
-
-    write(code_msg, "('code - ', i0, ',')") code
-
-    state = state_type(STDLIB_FS_ERROR, code_msg,a1,a2,a3,a4,a5,a6,a7,a8,&
-        a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19)
-end function FS_ERROR_CODE
-
-pure function FS_ERROR(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,&
-        a12,a13,a14,a15,a16,a17,a18,a19,a20) result(state)
-
-    type(state_type) :: state
-    !> Optional rank-agnostic arguments
-    class(*), intent(in), optional, dimension(..) :: a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,&
-        a11,a12,a13,a14,a15,a16,a17,a18,a19,a20
-
-    state = state_type(STDLIB_FS_ERROR, a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,&
-        a13,a14,a15,a16,a17,a18,a19,a20)
-end function FS_ERROR
-
-character function path_sep()
-    if (OS_TYPE() == OS_WINDOWS) then
-        path_sep = '\'
-    else
-        path_sep = '/'
-    end if
-end function path_sep
 
 end module stdlib_system
